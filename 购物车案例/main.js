a63676c075a37ed444d5d2003ccf057ed3e873a5
BossKing-newbie <724574109@qq.com>
2020-03-16 11:10:41 +0800

    购物车案例整理

---
+++ 购物车案例/main.js
@@ -0,0 +1,60 @@
+const app=new Vue({
+  el:'#app',
+  data:{
+    books:[
+      {
+        id: 1,
+        name: '《算法导论》',
+        date: '2006-9',
+        price: 85.00,
+        count: 1
+      },
+      {
+        id: 2,
+        name: '《Unix编程艺术》',
+        date: '2006-2',
+        price: 59.00,
+        count: 1
+      },
+      {
+        id: 3,
+        name: '《编程珠玑》',
+        date: '2006-10',
+        price: 39.00,
+        count: 1
+      },
+      {
+        id: 4,
+        name: '《代码大全》',
+        date: '2006-3',
+        price: 100.00,
+        count: 1
+      }
+    ]
+  },
+  methods:{
+    increment(index){
+      this.books[index].count++;
+    },
+    decrement(index){
+      this.books[index].count--;
+    },
+    removeHandler(index){
+      this.books.splice(index,1)
+    }
+  },
+  computed:{
+    getTotalPrice(){
+      let total=0;
+      for(let book of this.books){
+        total+=book.price*book.count;
+      }
+      return total;
+    }
+  },
+  filters:{
+    showPrice(price){
+      return '￥'+ price.toFixed(2)
+    }
+  }
+})
